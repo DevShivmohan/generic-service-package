3ea52f26069f4caa951732c1d340022bd599bdc9
Shivmohan <nshivmohan08@gmail.com>
2025-07-21 23:26:42 +0530

    added some impr

--- src/main/java/com/generic/service/config/SpringConfig.java
+++ src/main/java/com/generic/service/config/SpringConfig.java
@@ -1,8 +1,18 @@
 package com.generic.service.config;
 
+import com.generic.service.service.IdGenerationStrategy;
+import com.generic.service.service.impl.DefaultIdGenerationStrategy;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringConfig {
+
+    @Bean
+    @ConditionalOnMissingBean(IdGenerationStrategy.class)
+    public IdGenerationStrategy idGenerationStrategy() {
+        return new DefaultIdGenerationStrategy();
+    }
 
 }
