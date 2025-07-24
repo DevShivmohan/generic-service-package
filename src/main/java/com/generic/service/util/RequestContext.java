010006497e81f9030c866c3c015a54eb353b3057
Shivmohan <nshivmohan08@gmail.com>
2025-07-24 18:19:34 +0530

    added some impr

--- src/main/java/com/generic/service/util/RequestContext.java
+++ src/main/java/com/generic/service/util/RequestContext.java
@@ -4,14 +4,11 @@ import com.generic.service.model.GenericLoggedInUserData;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
 
-import java.util.Optional;
-
 public class RequestContext {
     public static GenericLoggedInUserData getUserFromRequestContextHolder() {
         final RequestAttributes attributes = RequestContextHolder.getRequestAttributes();
         if (attributes != null && attributes.getAttribute(GenericLoggedInUserData.class.getName(), RequestAttributes.SCOPE_REQUEST) != null) {
-            final GenericLoggedInUserData genericLoggedInUserData = attributes != null ? (GenericLoggedInUserData) attributes.getAttribute(GenericLoggedInUserData.class.getName(), RequestAttributes.SCOPE_REQUEST) : null;
-            return Optional.of(genericLoggedInUserData).orElse(new GenericLoggedInUserData());
+            return attributes != null ? (GenericLoggedInUserData) attributes.getAttribute(GenericLoggedInUserData.class.getName(), RequestAttributes.SCOPE_REQUEST) : new GenericLoggedInUserData();
         }
         return new GenericLoggedInUserData();
     }
