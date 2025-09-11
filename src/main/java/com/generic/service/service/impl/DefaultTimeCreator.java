be2192b37a89a889372e954a6b7034a57899d14c
Shivmohan <nshivmohan08@gmail.com>
2025-09-11 15:58:26 +0530

    added some impr

--- src/main/java/com/generic/service/service/impl/DefaultTimeCreator.java
+++ src/main/java/com/generic/service/service/impl/DefaultTimeCreator.java
@@ -1,13 +1,13 @@
 package com.generic.service.service.impl;
 
 import com.generic.service.service.GenericTimeCreator;
-import com.generic.service.util.GenericTimeUtil;
+import com.generic.service.util.GenericUtil;
 
 import java.time.LocalDateTime;
 
 public class DefaultTimeCreator implements GenericTimeCreator {
     @Override
     public LocalDateTime createLocalDateTime() {
-        return GenericTimeUtil.getLocalDateTimeInIST();
+        return GenericUtil.getLocalDateTimeInIST();
     }
 }
--- src/main/java/com/generic/service/util/GenericUtil.java
+++ src/main/java/com/generic/service/util/GenericUtil.java
@@ -1,15 +1,22 @@
 package com.generic.service.util;
 
+import com.fasterxml.uuid.Generators;
+
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
+import java.util.UUID;
 
-public class GenericTimeUtil {
+public class GenericUtil {
     public static LocalDateTime getLocalDateTimeInUTC() {
         return LocalDateTime.now(ZoneOffset.UTC);
     }
 
     public static LocalDateTime getLocalDateTimeInIST() {
         return LocalDateTime.now(ZoneId.of("Asia/Kolkata"));
+    }
+
+    public static UUID generateTimeBaseUUID() {
+        return Generators.timeBasedReorderedGenerator().generate();
     }
 }
