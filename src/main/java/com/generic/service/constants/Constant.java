c5557ee955c405ebf0baf846af9a3882e554dc15
Shivmohan <nshivmohan08@gmail.com>
2025-09-15 12:59:50 +0530

    added crypto service

---
+++ src/main/java/com/generic/service/constants/Constant.java
@@ -0,0 +1,6 @@
+package com.generic.service.constants;
+
+public class Constant {
+    public static final String X_GATEWAY_SECURITY_TOKEN = "x-gateway-token";
+    public static final String X_SERVICE_SECURITY_TOKEN = "x-service-token";
+}
